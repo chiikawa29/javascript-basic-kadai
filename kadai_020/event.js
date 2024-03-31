82f1d870ee84bebe921d89b0d56638d4797fa2bd
chiikawa29 <nii5quu@yahoo.co.jp>
2024-03-31 14:07:44 +0900

    DOM操作を使用して文字表示を変えよう

---
+++ kadai_020/event.js
@@ -0,0 +1,13 @@
+
+//textbtnはこれです
+const textbtn = document.getElementById('btn')
+
+//クリックした場合の処理
+textbtn.addEventListener('click',() =>{
+//チルドリストはクリエイトｈ2です
+ const remove =document.getElementById('text');
+ //チルドリスト文字列はこれにして
+    remove.textContent = 'ボタンをクリックし真下'
+    //テキストに書きくわえる
+    text.appendChild(childList);
+});
